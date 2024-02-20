0e0ae7879dc4187a96e3a8a3aa0ec8743db9985a
Infinite00loop <samriddhisingh07092002@gmail.com>
2024-02-20 13:30:31 +0530

    making app productin ready

--- controllers/expense.js
+++ controllers/expense.js
@@ -1,31 +1,51 @@
+const sequelize = require('../util/database');
+
 const Expense = require('../models/expense');
 
-exports.insertExpense = (req, res, next) => {
+exports.insertExpense = async (req, res, next) => {
+  const t = await sequelize.transaction();
+
+  try{
     let myObj=req.body;
-   req.user.createExpense(myObj)
-    .then(result=>{
-      req.user.update({totalexpense: (req.user.totalexpense || 0)+(+myObj.amount)})
+   const result= await req.user.createExpense(myObj, {transaction: t})
+   await req.user.update({totalexpense: (req.user.totalexpense || 0)+(+myObj.amount)}, {transaction: t})
+   await t.commit();
       console.log('Created expense');
       res.redirect('/get-expense')
-    })
-    .catch(err => {
-      console.log(err)
-    })
+
+  }
+  catch(err){
+    await t.rollback();
+     console.log(err)
+     res.status(500).json(err)
+  }
+    
   };
 
-exports.deleteExpense = (req,res,next)=>{
+exports.deleteExpense = async (req,res,next)=>{
+  const t = await sequelize.transaction();
+
+  try{
     const id=req.params.id;
-     Expense.destroy({
+    const amount=req.query.amount;
+    const expense= await Expense.destroy({
         where: {
           id: id,
           userdetailId: req.user.id
         },
-      })
-    .then((result)=>{
-        console.log(result);
+      },{transaction: t})
+    await req.user.update({totalexpense: (req.user.totalexpense)-(+amount)}, {transaction: t})
+    await t.commit();
+      console.log(expense);
         res.redirect('/get-expense')
-    })
-    .catch(err=>console.log(err));
+  }
+  catch(err){
+    await t.rollback();
+     console.log(err)
+     res.status(500).json(err)
+  }
+   
+    
 }
 
 exports.getExpense =(req,res,next)=>{
